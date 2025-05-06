b100f00c25d106ce6cdddb3ee4d2a410dbdca614
DevDuulat <duulat.bekm@mail.ru>
2025-05-06 11:31:09 +0600

    fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import react from "@astrojs/react";
 export default defineConfig({
   integrations: [tailwind(), react()],
   site: "https://devduulat.github.io",
-  base: "portfolio",
+  base: "/portfolio/",
   vite: {
     resolve: {
       alias: {
